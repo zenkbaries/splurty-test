7441c83d82cf91cda7b19a3a9e7ef976613ee5e0
John Towery <kbaries@gmail.com>
2017-05-10 23:07:14 -0600

    Add and configure Devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: 'jkt-splurty-test.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
